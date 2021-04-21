c0a1393e2848fba2de62e58e6f37a81ab416562e
WuHuaiSheng <zxc20044123@gmail.com>
2021-04-21 21:56:28 +0800

    add write

--- prodcuts.py
+++ prodcuts.py
@@ -7,8 +7,15 @@ while True:
     #p = [name, price]
     products.append([name, price])
 
-print(products) #印出大清單
+print(products) 
 print(products[0][1])
 
 for p in products:
-	print(p[0], '的價格是', p[1], '元') #印出小清單
+	print(p[0], '的價格是', p[1], '元') 
+sum = 0
+p = int(p)
+with open ('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '元' + '\n')
+		sum = sum + p[1]
+print(sum)
