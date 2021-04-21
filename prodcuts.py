581e7bcc2030d2675ad5d2e22073bf4f058d6b70
WuHuaiSheng <zxc20044123@gmail.com>
2021-04-21 22:03:54 +0800

    update

--- prodcuts.py
+++ prodcuts.py
@@ -4,18 +4,15 @@ while True:
     if name == 'q':
     	break
     price = input('請輸入商品價格: ')
+    price = int(price)
     #p = [name, price]
     products.append([name, price])
 
 print(products) 
-print(products[0][1])
-
 for p in products:
 	print(p[0], '的價格是', p[1], '元') 
-sum = 0
-p = int(p)
 with open ('products.csv', 'w') as f:
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '元' + '\n')
-		sum = sum + p[1]
-print(sum)
+		f.write(p[0] + ',' + str(p[1]) + '元' + '\n')
+
+
